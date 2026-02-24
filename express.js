e4eb0a4ef2246f1472480b5d6e632e869024a80b
krbck <burakkarabacak2002@gmail.com>
2026-02-24 12:55:03 +0300

    added json format

--- express.js
+++ express.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Pool } = require("pg");
 
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
